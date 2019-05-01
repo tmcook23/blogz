255fbeae6e89b38e3146b86cedd4b8ede168464f
Tracy Cook <tracymcook23@gmail.com>
2019-05-01 00:07:35 -0500

    Implement linked author from /blog page

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Blog(db.Model): #db is the object we created above
     title = db.Column(db.String(120))
     body = db.Column(db.String(21000))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    
     def __init__(self, title, body, owner):
         self.title = title
         self.body = body
